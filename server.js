d800e8b545cde1ec1fa7bafb6b564cadec6bfa6c
fabiovasques87 <fabioportovasques@yahoo.com.br>
2023-02-24 10:57:23 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+// import express, { Request, Response, ErrorRequestHandler } from 'express';
+// import path from 'path';
+
+require ('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileupload = require ('express-fileupload');
+
+//conexão com banco
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
+
+
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error',(error) =>{
+    console.log("erro: ", error.message);
+});
+
+
+
+const server = express();
+
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+server.use(fileupload());
+
+server.use(express.static(__dirname+'/public'));
+
+server.get('/ping', (req, res) => {
+    res.json({pong: true});
+});
+
+// server.use(express.static(path.join(__dirname, '../public')));
+// server.use(express.urlencoded({ extended: true }));
+
+
+server.listen(process.env.PORT, ()=> {
+    console.log(`Rodando no endereço: ${process.env.BASE} `);
+});
